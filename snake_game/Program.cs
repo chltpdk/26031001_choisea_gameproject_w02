1950c81cd1da39bfe8a7b26d0c397e853281d055
chltpdk <chltpdk1029@gmail.com>
2026-09-06 17:14:40 +0900

    Snake Game

---
+++ snake_game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace snake_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
